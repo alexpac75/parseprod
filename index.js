5e837989bfdb6e96a8e0ae249234468bbf3a3e89
Alex <alexandre@pretachanger.fr>
2016-03-31 11:55:12 +0200

    Mise en place certificat push ios

--- index.js
+++ index.js
@@ -19,7 +19,20 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  },
+    push: {
+    ios: [
+      {
+        pfx:  __dirname + '/certificats/pacpushdev.p12', // Dev PFX or P12
+        bundleId: 'com.excambia.pac',
+        production: false // Dev
+      },
+      {
+        pfx:  __dirname + '/certificats/pacprodpush.p12', // Prod PFX or P12
+            bundleId: 'com.excambia.pac',
+        production: true // Prod
+      }
+    ]
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
